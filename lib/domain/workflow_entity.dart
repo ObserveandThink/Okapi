09c8bf44d09221e2f0d6f0e454cce05edff8dce9
jimd <james_d_davis@outlook.com>
2024-10-07 21:43:31 -0600

    workflows use task_ids, not tasks

--- lib/domain/workflow_entity.dart
+++ lib/domain/workflow_entity.dart
@@ -1,13 +1,7 @@
-
-
 class Workflow {
   final String id;
   final String name;
-  final List<Task> tasks;
+  final List<String> taskIds;
 
-  Workflow({
-    required this.id,
-    required this.name,
-    required this.tasks,
-  });
+  Workflow({required this.id, required this.name, required this.taskIds});
 }
